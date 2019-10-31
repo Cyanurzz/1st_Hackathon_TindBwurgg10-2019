a50d75325a4a0f98c102965e45dcf74163bd70b6
Jean-christophe Rocher <jcrocher@orange.fr>
2019-10-31 12:04:35 +0100

    last commit

--- TindBwurgg/src/main/java/com/wildcodeschool/TindBwurgg/TindBwurggApplication.java
+++ TindBwurgg/src/main/java/com/wildcodeschool/TindBwurgg/TindBwurggApplication.java
@@ -12,13 +12,6 @@ public class TindBwurggApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TindBwurggApplication.class, args);
-
-		Film[] filmList = FilmFromJson.createFilms();
-		Monster[] monsterList = MonsterFromJson.createMonster();
-		System.out.println(filmList[3].getDirector());
-		System.out.println(monsterList[2].getName());
-
-
 	}
 
 }
